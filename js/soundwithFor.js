2e4a2dac853a6338d456eab6c1f5c077e4f6eedc
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-06-22 20:00:23 -0300

    Feat: criando a mesma estrutura para o for

---
+++ js/soundwithFor.js
@@ -0,0 +1,22 @@
+function playSong(idButtonMusica) {
+
+    document.querySelector(idButtonMusica).play();
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//document.querySelector('.tecla_pom').onclick = playSong;
+
+//Percorrendo a lista
+for (let cont = 0; cont < listaDeTeclas.length; cont++) {
+
+    const tecla = listaDeTeclas[cont];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function () {
+        playSong(idAudio);
+    }
+}
+
